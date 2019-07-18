7fd82b384309cd9391ea88d1018cb447f128291d
agneska <agneska@agneska.fr>
2019-07-18 16:10:00 +0200

    add shell script to download html files

---
+++ wget.sh
@@ -0,0 +1,8 @@
+# DOWNLOAD="wget --no-check-certificate"
+DOWNLOAD="curl --insecure"
+REP="HTML"
+mkdir $REP
+for i in {1957..2019}
+do
+  $DOWNLOAD "http://megalotto.pl/wyniki/lotto/losowania-z-roku-$i" > "$REP/$i-wyniki.html"
+done
